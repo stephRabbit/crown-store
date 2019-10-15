934fc45b2cdcdccd1ddd4d3fe2ab05068459dca1
Steph <N/A>
2019-10-14 21:12:33 -0400

    menu hover state

--- src/components/menu-item/index.js
+++ src/components/menu-item/index.js
@@ -4,12 +4,13 @@ import './menu-item.scss'
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className={`${size} menu-item`}>
+    <div className={`${size} menu-item`}>
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className='background-image'></div>
       <div className='content'>
         <h1 className='title'>{title}</h1>
-        <span className='sub-title'>Shop Now</span>
+        <span className='subtitle'>Shop Now</span>
       </div>
     </div>
   )
