549facacd4919597b31abea85ab2515eba404a16
Steph <N/A>
2019-10-20 23:09:37 -0400

    Cart item component and redux flow

--- src/components/cart-dropdown/index.js
+++ src/components/cart-dropdown/index.js
@@ -1,16 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import CustomButton from '../custom-button'
+import CartItem from '../cart-item'
 
 import './cart-dropdown.scss'
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className='cart-dropdown'>
-      <div className='cart-item'></div>
+      <div className='cart-items'>
+        {cartItems.map(item => (
+          <CartItem key={item.id} item={item} />
+        ))}
+      </div>
       <CustomButton inverted>Go to Checkout</CustomButton>
     </div>
   )
 }
 
-export default CartDropdown
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown)
